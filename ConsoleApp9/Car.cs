feae4c29cbf4335bedb15812f8cb8b16f740558b
AkbarGM <56382572+AkbarGM@users.noreply.github.com>
2020-02-10 14:27:23 +0500

    Add project files.

---
+++ ConsoleApp9/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Car
+    {
+        public string type;
+        public string name;
+        public string version;
+        public string color;
+        public int doorsCount;
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"name: {name}\n" +
+                $"version: {version}\n"+
+                $"type: {type}\n"+
+                $"color: {color}\n"+
+                $"doorsCount: {doorsCount}\n");
+        }
+    }
+}
---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car mers = new Car();
+            mers.name = "Mercedes";
+            mers.version = "CLS 220";
+            mers.type = "sedan";
+            mers.color = "white";
+            mers.doorsCount = 4;
+            mers.GetInfo();
+
+
+            Car bmw = new Car();
+            bmw.name = "BMW";
+            bmw.version = "X6";
+            bmw.type = "SUV";
+            bmw.color = "black";
+            bmw.doorsCount = 5;
+            bmw.GetInfo();
+
+            Console.ReadLine();
+
+        }
+    }
+}
